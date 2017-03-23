e3d07ad0d82982dc98fcd387d9e2043c74393c28
itisha-23 <itishagupta.123@gmail.com>
2017-03-24 00:25:57 +0530

    Add files via upload

---
+++ #1ds1traversalofbinarytree.c
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+struct node{
+          int data;
+        struct node *left;
+        struct node *right;
+          };
+typedef struct node tnode;
+int main()
+{
+    cout<<"Binary tree traversal\n";
+    cout<<"1.creation of tree\n";
+    cout<<"2.preorder traversal\n";
+    cout
+
+}
+
+
